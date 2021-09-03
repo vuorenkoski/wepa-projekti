052f6e8e796d1c68f630192332ce37753753f833
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2021-09-03 11:00:51 +0300

    fix for production conf

--- src/main/java/projekti/ProductionSecurityConfiguration.java
+++ src/main/java/projekti/ProductionSecurityConfiguration.java
@@ -23,12 +23,18 @@ public class ProductionSecurityConfiguration extends WebSecurityConfigurerAdapte
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        
+        // poistetaan csrf-tarkistus käytöstä h2-konsolin vuoksi
+//        http.csrf().disable();
+//        http.headers().frameOptions().sameOrigin();
+
         http.formLogin().loginPage("/login").defaultSuccessUrl("/");
         http.logout().logoutSuccessUrl("/");
         
         http.authorizeRequests()
-                .antMatchers("/signup","/","/styles.css", "login").permitAll()
+                .antMatchers("/signup","/","/styles.css", "/login").permitAll()
+//                .antMatchers("/h2-console","/h2-console/**").permitAll()
                 .anyRequest().authenticated().and()
+                .formLogin().permitAll().and()
                 .logout().permitAll();
     }
 
